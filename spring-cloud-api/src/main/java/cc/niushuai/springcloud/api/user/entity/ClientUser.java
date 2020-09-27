8d90b6f6f5d77f1135e1dfbf3c7daedb747e6fe1
niushuai <niushuai951101@gmail.com>
2020-09-27 15:34:49 +0800

    fix

--- spring-cloud-api/src/main/java/cc/niushuai/springcloud/api/user/entity/ClientUser.java
+++ spring-cloud-api/src/main/java/cc/niushuai/springcloud/api/user/entity/ClientUser.java
@@ -1,9 +1,13 @@
 package cc.niushuai.springcloud.api.user.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Accessors(chain = true)
 public class ClientUser {
 
